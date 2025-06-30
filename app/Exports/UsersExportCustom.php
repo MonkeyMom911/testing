f3f35c038f48bc8867190e7b9b27c4cb7176a1ab
adit_rmdhn <madityar0443@gmail.com>
2025-06-30 11:50:17 +0700

    Add files via upload

---
+++ app/Exports/UsersExportCustom.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class UsersExportCustom implements FromCollection, WithHeadings, WithMapping
+{
+    protected $users;
+
+    public function __construct($users)
+    {
+        $this->users = $users;
+    }
+
+    public function collection()
+    {
+        return $this->users;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Name',
+            'Email',
+            'Role',
+            'Status',
+            'Phone',
+            'Joined At',
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->name,
+            $user->email,
+            $user->role,
+            $user->email_verified_at ? 'Active' : 'Inactive',
+            $user->phone_number ?? 'N/A',
+            $user->created_at->format('d M Y'),
+        ];
+    }
+}
